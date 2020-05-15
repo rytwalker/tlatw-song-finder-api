dff10a41425fc87add568b3580a7b1f4ea50dfff
Ryan Walker <rytwalker@gmail.com>
2020-05-15 09:29:46 -0400

    getting response back from spotify

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = require("./routes/index");
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
 app.get("/", (_req, res) => {
   res.json({ message: "hello" });
--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -1,9 +1,40 @@
 const express = require("express");
-
+const fetch = require("node-fetch");
+const { URLSearchParams } = require("url");
+const SpotifyWebApi = require("spotify-web-api-node");
+const btoa = require("btoa");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "spotify page" });
+router.get("/", async (_req, res) => {
+  const clientId = "52f003f8b2e543b3bec5cc38f3bb3f54";
+  const clientSecret = "34f406ea24f44102a04411f922361984";
+  const headerToken = btoa(clientId + ":" + clientSecret);
+  const params = new URLSearchParams("grant_type=client_credentials");
+  try {
+    const authResponse = await fetch("https://accounts.spotify.com/api/token", {
+      method: "post",
+      body: params.toString(),
+      headers: {
+        Authorization: `Basic ${headerToken}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const { access_token } = await authResponse.json();
+    const response = await fetch(
+      "https://api.spotify.com/v1/artists/6ls5A8Wys9Swixpz4v6kj3/albums",
+      {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+    const jsonRes = await response.json();
+    res.status(200).json(jsonRes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
