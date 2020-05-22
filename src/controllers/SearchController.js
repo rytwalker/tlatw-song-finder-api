aeb8a465c4eeaf077b8baac4a309c2a672618824
Ryan Walker <rytwalker@gmail.com>
2020-05-22 16:37:44 -0400

    cleaned up query action

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,30 +1,11 @@
 const Track = require("../models/Track");
-const Album = require("../models/Album");
-
-function hasKey(key, keys) {
-  return keys.includes(key);
-}
-
-function sortByParam(trackArr, trackValue, param) {
-  return trackArr.sort(
-    (a, b) => Math.abs(a[trackValue] - param) - Math.abs(b[trackValue] - param)
-  );
-}
-
-function addScore(trackArr, scoreKey) {
-  return trackArr.map((track, index) => ({
-    ...track,
-    [scoreKey]: index + 1,
-  }));
-}
-
-async function addAlbum(track) {
-  const album = await Album.find({ id: track.albumId });
-  track = await {
-    ...track,
-    album,
-  };
-}
+const {
+  filterTempo,
+  filterKey,
+  filterMode,
+  addAudioFeature,
+  scoreSort,
+} = require("../helpers/searchHelpers");
 
 class SearchController {
   static async query(req, res) {
@@ -43,77 +24,48 @@ class SearchController {
       const tracks = await Track.find();
 
       // FILTERS
-      const tempoFilteredTracks = tracks.filter(
-        (track) => track.tempo > tempo[0] && track.tempo < tempo[1]
-      );
-      const keyFilteredTracks = tempoFilteredTracks.filter((track) =>
-        hasKey(track.key, keys)
-      );
-
-      const modeFilteredTracks = keyFilteredTracks.filter((track) => {
-        if (mode.length === 2) {
-          return true;
-        }
-
-        return mode[0] === track.mode;
-      });
+      const filteredTracks = tracks
+        .filter((track) => filterTempo(track.tempo, tempo))
+        .filter((track) => filterKey(track.key, keys))
+        .filter((track) => filterMode(track.mode, mode));
 
       // SORTS and SCORING
-      const happinessSort = sortByParam(
-        modeFilteredTracks,
+      const withHappiness = addAudioFeature(
+        filteredTracks,
+        happiness,
         "valence",
-        happiness
+        "happinessScore"
       );
-      const addHappinessScore = addScore(happinessSort, "happinessScore");
 
-      const energySort = sortByParam(addHappinessScore, "energy", energy);
-      const addEnergyScore = addScore(energySort, "energyScore");
+      const withEnergy = addAudioFeature(
+        withHappiness,
+        energy,
+        "energy",
+        "energyScore"
+      );
 
-      const danceabilitySort = sortByParam(
-        addEnergyScore,
+      const withDanceability = addAudioFeature(
+        withEnergy,
+        danceability,
         "danceability",
-        danceability
-      );
-      const addDanceabilityScore = addScore(
-        danceabilitySort,
         "dancabilityScore"
       );
 
-      const acousticnessSort = sortByParam(
-        addDanceabilityScore,
+      const withAcousticness = addAudioFeature(
+        withDanceability,
+        acousticness,
         "acousticness",
-        acousticness
-      );
-      const addAcousticnessScore = addScore(
-        acousticnessSort,
         "acousticnessScore"
       );
 
-      const instrumentalnessSort = sortByParam(
-        addAcousticnessScore,
+      const withInstrumentalness = addAudioFeature(
+        withAcousticness,
+        instrumentalness,
         "instrumentalness",
-        instrumentalness
-      );
-      const addInstrumentalnessScore = addScore(
-        instrumentalnessSort,
         "instrumentalnessScore"
       );
 
-      const sortedTracks = addInstrumentalnessScore.sort(
-        (a, b) =>
-          a.happinessScore +
-          a.energyScore +
-          a.dancabilityScore +
-          a.acousticnessScore +
-          a.instrumentalnessScore -
-          (b.happinessScore +
-            b.energyScore +
-            b.dancabilityScore +
-            b.acousticnessScore +
-            a.instrumentalnessScore)
-      );
-
-      // sortedTracks.forEach(addAlbum);
+      const sortedTracks = withInstrumentalness.sort(scoreSort);
 
       res.status(200).json({
         length: sortedTracks.length,
