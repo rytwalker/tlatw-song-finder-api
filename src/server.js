6754224cc8c405ae61afb222d3cb7e40e33e32b6
Ryan Walker <rytwalker@gmail.com>
2020-07-03 16:20:47 -0400

    actually using the dynamic port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("\n", chalk.blueBright("*********************************"));
   console.log(` App is running on Port: ${chalk.blue.underline(PORT)}`);
   console.log(chalk.blueBright(" *********************************"), "\n");
