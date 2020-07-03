443a25cbad2d2eb56b89e3340f090287550df895
Ryan Walker <rytwalker@gmail.com>
2020-07-03 16:04:10 -0400

    getting setup for prod

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     client: "pg",
@@ -25,7 +27,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + "?ssl=true",
     migrations: {
       directory: "./src/db/migrations",
     },
