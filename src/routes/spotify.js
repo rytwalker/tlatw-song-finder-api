28196f76f4f7d50a01dfc85e4233637d9a8c60cf
Ryan Walker <rytwalker@gmail.com>
2020-07-03 19:14:13 -0400

    set up spotify endpoint to get artist

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -8,87 +8,109 @@ const AudioFeature = require("../models/AudioFeature");
 
 const router = express.Router();
 
+const TLATW_SPOTIFY_ID = "6ls5A8Wys9Swixpz4v6kj3";
+
 router.get("/", authSpotify, async (_req, res) => {
   if (!res.access_token) {
     return res.status(400).json({ message: "bad request" });
   }
-  // 1. Get Artist and save to DB
   try {
+    // Since we only have one artist at this point it just checks if there are any in the db
+    const hasArtist = await Artist.find();
+    if (!hasArtist.length) {
+      return res.status(400).json({ message: "Artist already exists in DB." });
+    }
+
+    // 1. Get Artist and save to DB
+    const artistResponse = await baseRequest({
+      slug: `artists/${TLATW_SPOTIFY_ID}`,
+      access_token: res.access_token,
+    });
+    const jsonArtistRes = await artistResponse.json();
+
+    await Artist.insert({
+      name: jsonArtistRes.name,
+      spotifyId: jsonArtistRes.id,
+      spotifyUrl: jsonArtistRes.external_urls.spotify,
+      followers: jsonArtistRes.followers.total,
+    });
+
+    // 2. Albums
+    const albumResponse = await baseRequest({
+      slug: `albums/${TLATW_SPOTIFY_ID}`,
+      access_token: res.access_token,
+    });
+    const jsonAlbumRes = await albumResponse.json();
+
+    jsonAlbumRes.items.forEach(async (item) => {
+      if (item.available_markets.find((market) => market === "US")) {
+        await Album.insert({
+          name: item.name,
+          spotifyId: item.id,
+          spotifyUrl: item.external_urls.spotify,
+          artistId: 1,
+          albumType: item.album_type,
+          releaseDate: item.release_date,
+          largeImageUrl: item.images[0].url,
+          smallImageUrl: item.images[1].url,
+          thumbnailUrl: item.images[2].url,
+          totalTracks: item.total_tracks,
+        });
+        // returnIds.push(id);
+      }
+    });
+
+    // 3. Tracks
+    const albums = await Album.find();
+    albums.forEach(async (album) => {
+      const tracksResponse = await baseRequest({
+        slug: `albums/${album.spotifyId}/tracks`,
+        access_token: res.access_token,
+      });
+
+      const jsonTracksRes = await tracksResponse.json();
+      jsonTracksRes.items.forEach(async (track) => {
+        await Track.insert({
+          name: track.name,
+          spotifyId: track.id,
+          spotifyUrl: track.external_urls.spotify,
+          albumId: album.id,
+          trackNumber: track.track_number,
+          durationMs: track.duration_ms,
+          previewUrl: track.preview_url,
+        });
+      });
+    });
+
+    // 4. Audio Features
     const tracks = await Track.find();
     tracks.forEach(async (track) => {
-      const response = await baseRequest({
+      const audioFeatureResponse = await baseRequest({
         slug: `audio-features/${track.spotifyId}`,
         access_token: res.access_token,
       });
-      const jsonRes = await response.json();
-
+      const jsonAudioFeatureRes = await audioFeatureResponse.json();
       await AudioFeature.insert({
-        spotifyId: jsonRes.id,
-        spotifyUrl: jsonRes.uri,
+        spotifyId: jsonAudioFeatureRes.id,
+        spotifyUrl: jsonAudioFeatureRes.uri,
         trackId: track.id,
-        danceability: jsonRes.danceability,
-        energy: jsonRes.energy,
-        key: jsonRes.key,
-        loudness: jsonRes.loudness,
-        mode: jsonRes.mode,
-        speechiness: jsonRes.speechiness,
-        acousticness: jsonRes.acousticness,
-        instrumentalness: jsonRes.instrumentalness,
-        liveness: jsonRes.liveness,
-        valence: jsonRes.valence,
-        tempo: jsonRes.tempo,
-        timeSignature: jsonRes.time_signature,
-        trackHref: jsonRes.track_href,
-        analysisUrl: jsonRes.analysis_url,
+        danceability: jsonAudioFeatureRes.danceability,
+        energy: jsonAudioFeatureRes.energy,
+        key: jsonAudioFeatureRes.key,
+        loudness: jsonAudioFeatureRes.loudness,
+        mode: jsonAudioFeatureRes.mode,
+        speechiness: jsonAudioFeatureRes.speechiness,
+        acousticness: jsonAudioFeatureRes.acousticness,
+        instrumentalness: jsonAudioFeatureRes.instrumentalness,
+        liveness: jsonAudioFeatureRes.liveness,
+        valence: jsonAudioFeatureRes.valence,
+        tempo: jsonAudioFeatureRes.tempo,
+        timeSignature: jsonAudioFeatureRes.time_signature,
+        trackHref: jsonAudioFeatureRes.track_href,
+        analysisUrl: jsonAudioFeatureRes.analysis_url,
       });
     });
-    // const albums = await Album.find();
-    // albums.forEach(async (album) => {
-    //   const response = await baseRequest({
-    //     slug: `albums/${album.spotifyId}/tracks`,
-    //     access_token: res.access_token,
-    //   });
-    //   const jsonRes = await response.json();
-    //   jsonRes.items.forEach(async (track) => {
-    //     await Track.insert({
-    //       name: track.name,
-    //       spotifyId: track.id,
-    //       spotifyUrl: track.external_urls.spotify,
-    //       albumId: album.id,
-    //       trackNumber: track.track_number,
-    //       durationMs: track.duration_ms,
-    //       previewUrl: track.preview_url,
-    //     });
-    //   });
-    // });
-    // jsonRes.items.forEach(async (item) => {
-    //   if (item.available_markets.find((market) => market === "US")) {
-    //     const id = await Album.insert({
-    //       name: item.name,
-    //       spotifyId: item.id,
-    //       spotifyUrl: item.external_urls.spotify,
-    //       artistId: 1,
-    //       albumType: item.album_type,
-    //       releaseDate: item.release_date,
-    //       largeImageUrl: item.images[0].url,
-    //       smallImageUrl: item.images[1].url,
-    //       thumbnailUrl: item.images[2].url,
-    //       totalTracks: item.total_tracks,
-    //     });
-    //     returnIds.push(id);
-    //   }
-    // });
-    // const returnId = await Artist.insert({
-    //   name: jsonRes.name,
-    //   spotifyId: jsonRes.id,
-    //   spotifyUrl: jsonRes.external_urls.spotify,
-    //   followers: jsonRes.followers.total,
-    // });
-    // const response = await baseRequest({
-    //   slug: `audio-features/3wvvtB5FeboO9piUEwLkCK`,
-    //   access_token: res.access_token,
-    // });
-    // const jsonRes = await response.json();
+
     res.status(200).json({ message: "success" });
   } catch (error) {
     console.log(error);
